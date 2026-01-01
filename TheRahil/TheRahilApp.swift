89d18bcd680fb2869ffc5fd6b166e14a7bbe9fa2
Alireza Hashemi <jelavor@gamil.com>
2026-01-01 04:59:53 +0330

    Initial Commit

---
+++ TheRahil/TheRahilApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheRahilApp.swift
+//  TheRahil
+//
+//  Created by Alireza Hashemi on 2026/1/1.
+//
+
+import SwiftUI
+
+@main
+struct TheRahilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
